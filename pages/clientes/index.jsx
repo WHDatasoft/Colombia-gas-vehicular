2e5b86e39d5eee9d85699b4c1ba0bc2ddbf99ad8
Sebas-Ar <juan_ariasd@hotmail.com>
2021-11-30 11:03:11 -0500

    testing

--- pages/clientes/index.jsx
+++ pages/clientes/index.jsx
@@ -2,6 +2,7 @@ import MainFooter from '../../components/Footer/MainFooter'
 import Head from '../../components/Head/Head'
 import ClientMain from '../../components/Sections/ClientMain'
 import SuccessStories from '../../components/Sections/SuccessStories'
+import SlideSecond from '../../components/slides-didi/SlideSecond'
 import MainLayout from "../../layout/MainLayout"
 
 const Index = () => {
@@ -10,16 +11,15 @@ const Index = () => {
 			<title>Colombia Gas Vehicular</title>
 		</Head>
 
-		<ClientMain
-			urlImg="car"
-			backgroundColor="#1c580dAA"
-			urlImgFinal="aliance"
-			rowGap="3"
-		>
-			<h2>TRABAJA CON NOSOTROS</h2>
-			<h1>GAS NATURAL VEHICULAR</h1>
-			<img src="/icon/aliance.svg" alt="" className="final" />
-		</ClientMain>
+		<div className="wrapper">
+			<SlideSecond
+				backgroundImg="car"
+				color="#1c580dAA"
+				mainTitle="TRABAJA CON NOSOTROS"
+				secondTitle="GRATIS"
+				img="/icon/aliance.svg"
+			/>,
+		</div>
 
 		<br /><br />
 
@@ -30,28 +30,9 @@ const Index = () => {
 
 		<style jsx>{`
 
-			h1, h2 {
-				text-align: center;
-			}
 
-			h2 {
-				color: white;
-				font-size: 5rem;
-				font-weight: 600;
-				max-width: 540px;
-				letter-spacing: 0.2rem;
-			}
-
-			h1 {
-				color: var(--light-green);
-				background-color: white;
-				padding: .5rem 1rem;
-				font-size: 2rem;
-				letter-spacing: 0.1rem;
-			}
-		
-			img {
-				height: 4rem;
+			.wrapper {
+				height: 80vh;
 			}
 		
 		`}</style>
