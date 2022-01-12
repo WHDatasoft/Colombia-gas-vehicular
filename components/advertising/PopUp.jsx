e81e0dc3122bfe14d20a57c131467395204f8397
Sebas-Ar <juan_ariasd@hotmail.com>
2022-01-12 16:57:49 -0500

    creo que ya

--- components/advertising/PopUp.jsx
+++ components/advertising/PopUp.jsx
@@ -39,7 +39,7 @@ const PopUp = () => {
 
 			<div className="advertising">
 
-				<button onClick={toggleEnable}>
+				<button className="close" onClick={toggleEnable}>
 					<svg viewBox="0 0 512 512">
 						<path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm101.8-262.2L295.6 256l62.2 62.2c4.7 4.7 4.7 12.3 0 17l-22.6 22.6c-4.7 4.7-12.3 4.7-17 0L256 295.6l-62.2 62.2c-4.7 4.7-12.3 4.7-17 0l-22.6-22.6c-4.7-4.7-4.7-12.3 0-17l62.2-62.2-62.2-62.2c-4.7-4.7-4.7-12.3 0-17l22.6-22.6c4.7-4.7 12.3-4.7 17 0l62.2 62.2 62.2-62.2c4.7-4.7 12.3-4.7 17 0l22.6 22.6c4.7 4.7 4.7 12.3 0 17z"></path>
 					</svg>
@@ -49,7 +49,11 @@ const PopUp = () => {
 						?
 						<FormAdvertising setIsEnable={setIsEnable} />
 						:
-						<img onClick={() => setEnableForm(true)} src={image} alt="" />
+						<>
+							<img src="https://colombiagasvehicular.s3.amazonaws.com/advertising/Pieza-Gr%C3%A1fica-de-referidos-g-sdd_Mesa-de-trabajo-1.png" alt="" />
+							{/* <img src={image} alt="" /> */}
+							<button className="register" onClick={() => setEnableForm(true)}>Registrate</button>
+						</>
 				}
 			</div>
 
@@ -71,12 +75,29 @@ const PopUp = () => {
 					position: relative;
 				}
 
-				img {
-					height: 70vmin;
+				.register {
+					position: absolute;
 					cursor: pointer;
+					z-index: 10;
+					bottom: -3rem;
+					left: 50%;
+					transform: translateX(-50%);
+					padding: .5rem 1rem;
+					background-color: unset;
+					color: white;
+					font-weight: 600;
+					font-size: 1.4rem;
+					border: 1px solid white;
+					border-radius: 2rem;
+					text-transform: uppercase;
+					animation: test 1s forwards;
 				}
 
-				button {
+				img {
+					height: 70vmin;
+				}
+
+				.close {
 					position: absolute;
 					color: ${enableForm ? 'white' : 'var(--blue)'};
 					background-color: unset;
@@ -87,6 +108,16 @@ const PopUp = () => {
 
 				svg {
 					height: 2rem;
+				}
+
+				@keyframes test {
+					from {
+						bottom: -3rem
+					}
+
+					to {
+						bottom: 1rem
+					}
 				}
 
 			`}</style>
