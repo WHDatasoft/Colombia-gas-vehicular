3eec4feba34486a403a724a22fac796a50225cb4
Wilsonsoft <114179313+WHDatasoft@users.noreply.github.com>
2024-05-20 20:52:38 -0500

    Update Whatsapp.jsx

--- components/social/Whatsapp.jsx
+++ components/social/Whatsapp.jsx
@@ -8,7 +8,7 @@ const Whatsapp = () => {
             <p>Chatea con nosotros</p>
         </div>
         <button>
-            <a href="https://wa.me/message/UPI3PA7IC3OLN1" target="_blank" rel="noopener noreferrer">
+            <a href="https://wa.me/573242578287">
                 <img src="/icon/whatsapp.svg" alt="icono de contacto por whatsapp" />
             </a>
 
